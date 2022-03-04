ef5b15f2527ade56311e7ab464a35e2793fbe0d9
Mohammed Shabz <coolshakeeb@gmail.com>
2022-03-04 15:19:20 +0100

    Added helper methods to Direction

--- src/ai/gr64/Data/Enums/Direction.java
+++ src/ai/gr64/Data/Enums/Direction.java
@@ -9,12 +9,37 @@ public enum Direction {
     LEFT(5);
 
     private final int value;
-    private Direction(int value){
+
+    private Direction(int value) {
         this.value = value;
     }
 
     public int getValue() {
         return value;
+    }
+
+    public static Direction fromValue(int value) {
+        switch (value) {
+            case 0:
+                return Direction.UP_LEFT;
+            case 1:
+                return Direction.UP_RIGHT;
+            case 2:
+                return Direction.RIGHT;
+            case 3:
+                return Direction.DOWN_RIGHT;
+            case 4:
+                return Direction.DOWN_LEFT;
+            case 5:
+                return Direction.LEFT;
+            default:
+                throw new IndexOutOfBoundsException();
+        }
+
+    }
+
+    public static Direction opposite(Direction dir) {
+        return fromValue((dir.getValue() + 3) % 6);
     }
 
 }
