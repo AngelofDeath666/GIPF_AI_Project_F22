f63f9fe7a9669f34c6ac57b12d1a244f1bc68b34
Eraisuithon <mj@lpj-software.dk>
2022-03-09 14:19:44 +0100

    Added switch to choose between unicode and ascii chars for text-ui

--- src/ai/gr64/UI/ASCIIUI.java
+++ src/ai/gr64/UI/ASCIIUI.java
@@ -1,10 +1,33 @@
 package ai.gr64.UI;
 
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+
 import ai.gr64.Data.Interfaces.IUI;
 import ai.gr64.Engine.DTOs.GameState;
 import ai.gr64.Engine.DTOs.Move;
 
 public class ASCIIUI implements IUI {
+    boolean useUnicode = false;
+
+    public ASCIIUI(boolean useUnicode) {
+        if (useUnicode) {
+            try {
+                System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out), true, "UTF-8"));
+                useUnicode = true;
+            } catch (UnsupportedEncodingException e) {
+                useUnicode = false;
+            }
+        }
+
+        if (this.useUnicode){
+            //set characters
+        } else {
+            //set ascii characters
+        }
+    }
 
     @Override
     public Move GetPlayerInput() {
@@ -15,7 +38,7 @@ public class ASCIIUI implements IUI {
     @Override
     public void UpdateUi(GameState state) {
         // TODO Auto-generated method stub
-        
+
     }
-    
+
 }
