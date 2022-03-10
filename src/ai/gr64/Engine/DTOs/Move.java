ac456889c43e3dd2dbafd23c83b93fb9f578ad81
Mohammed Shabz <coolshakeeb@gmail.com>
2022-03-10 14:49:00 +0100

    Updated the move DTO

--- src/ai/gr64/Engine/DTOs/Move.java
+++ src/ai/gr64/Engine/DTOs/Move.java
@@ -1,6 +1,12 @@
 package ai.gr64.Engine.DTOs;
 
-//To be implemented, should be a DTO representing a single players turn in the game, aka. a move
+import ai.gr64.Data.Enums.Direction;
+import ai.gr64.Data.Enums.Piece;
+
+//DTO representing a move
 public class Move {
-    
+    private Piece piece;
+    private int placementNode;
+    private Direction direction;
+
 }
