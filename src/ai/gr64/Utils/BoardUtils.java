8a168db0acdc8bfebc0bec5c0caa3b76f3382d2a
Eraisuithon <mj@lpj-software.dk>
2022-03-11 14:49:41 +0100

    Refactored StateFactory to be more readable, and converted a calculation to a cached util

--- src/ai/gr64/Utils/BoardUtils.java
+++ src/ai/gr64/Utils/BoardUtils.java
@@ -1,12 +1,42 @@
 package ai.gr64.Utils;
 
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Map;
+
 import ai.gr64.Engine.DTOs.GameState;
 import ai.gr64.Engine.DTOs.Move;
 
 public class BoardUtils {
     
+    
     // To be implemented, should return an array of all possible moves to be made based on the given GameState
     public static Move[] GetAllMoves(GameState state) {
         return null;
+    }
+
+    private static Map<Integer, int[]> cachedLayers = new HashMap<Integer, int[]>();
+
+    public static int[] LayerEnds (int layers){
+        if (cachedLayers.containsKey(layers))
+            return cachedLayers.get(layers);
+
+        int[] layerEnds = new int[2 * layers + 3];
+
+        //Loop to calculate each of the layer ends
+        //The first has a specific value, and the rest are calculated based on the last value plus the amount of nodes in the current row
+        for (int i = 0; i < layerEnds.length; i++) {
+            if (i == 0) {
+                layerEnds[0] = layers + 2;
+
+            } else if (i <= layers + 1) {
+                layerEnds[i] = layerEnds[i - 1] + layers + 2 + i;
+
+            } else {
+                layerEnds[i] = layerEnds[i - 1] + layers + 2 + (layers * 2 + 3) - i - 1;
+            }
+        }
+        cachedLayers.put(layers, layerEnds);
+        return layerEnds;
     }
 }
--- src/ai/gr64/Utils/StateFactory.java
+++ src/ai/gr64/Utils/StateFactory.java
@@ -17,97 +17,13 @@ public class StateFactory {
         INode[] nodes = new INode[numNodes];
 
         //layer ends are the 1-indexed index of the last node in each layer/row
-        int[] layerEnds = new int[2 * layers + 3];
+        int[] layerEnds = BoardUtils.LayerEnds(layers);
 
-        //Loop to calculate each of the layer ends
-        //The first has a specific value, and the rest are calculated based on the last value plus the amount of nodes in the current row
-        for (int i = 0; i < layerEnds.length; i++) {
-            if (i == 0) {
-                layerEnds[0] = layers + 2;
+        GenerateNodes(nodes, layerEnds);
 
-            } else if (i <= layers + 1) {
-                layerEnds[i] = layerEnds[i - 1] + layers + 2 + i;
+        AddNeighbors(nodes, layerEnds);
 
-            } else {
-                layerEnds[i] = layerEnds[i - 1] + layers + 2 + (layers * 2 + 3) - i - 1;
-            }
-        }
-
-        //Generates the specific nodes and puts them in the nodes array
-        //uses a method to calculate whether its an outer or inner node
-        for (int i = 0; i < numNodes; i++) {
-            if (OuterNode(i, layerEnds)) {
-                nodes[i] = new OuterNode();
-
-            } else {
-                nodes[i] = new InnerNode();
-            }
-        }
-
-        //Loops through all the nodes and adds the neighbors on each side
-        for (int i = 0; i < numNodes; i++) {
-            if (nodes[i] instanceof OuterNode) {
-                continue;
-            }
-            //Some math calculation as diagonal offsets change when the row-width starts to shrink again
-            int currentLayer = GetLayer(i, layerEnds) - 1;
-            int size = layerEnds[currentLayer] - layerEnds[currentLayer - 1];
-            int upModifier = (currentLayer > layerEnds.length/2 ? 1 : 0);
-            int downModifier = (currentLayer >= layerEnds.length/2 ? 1 : 0);
-            if (!nodes[i].hasNeighbor(Direction.LEFT)) {
-                nodes[i].addNeighbor(nodes[i - 1], Direction.LEFT);
-            }
-            if (!nodes[i].hasNeighbor(Direction.RIGHT)) {
-                nodes[i].addNeighbor(nodes[i + 1], Direction.RIGHT);
-            }
-            if (!nodes[i].hasNeighbor(Direction.UP_LEFT)) {
-                nodes[i].addNeighbor(nodes[i - size - upModifier], Direction.UP_LEFT);
-            }
-            if (!nodes[i].hasNeighbor(Direction.UP_RIGHT)) {
-                nodes[i].addNeighbor(nodes[i - size + 1 - upModifier], Direction.UP_RIGHT);
-            }
-            if (!nodes[i].hasNeighbor(Direction.DOWN_LEFT)) {
-                nodes[i].addNeighbor(nodes[i + size - downModifier], Direction.DOWN_LEFT);
-            }
-            if (!nodes[i].hasNeighbor(Direction.DOWN_RIGHT)) {
-                nodes[i].addNeighbor(nodes[i + size + 1 - downModifier], Direction.DOWN_RIGHT);
-            }
-
-        }
-
-        /*Used to specify the starting mode of the game:
-            None:
-                there will be placed no pieces on the board to start with
-            Normal:
-                in each "corner" of the board, there will be either a black or a white piece
-            Double:
-                in each "corner" of the board there will be a GIPF piece either black or white
-        */
-        switch (startingMode) {
-            //No pieces have to be placed
-            case NONE:
-                break;
-
-            //Finds each corner and places a normal piece
-            case NORMAL:
-                for (int i = 0; i < 6; i++) {
-                    INode workingNode = nodes[numNodes / 2];
-
-                    for (int j = 0; j < layers; j++) {
-                        workingNode = workingNode.neighbor(Direction.fromValue(i));
-                    }
-                    if (i % 2 == 0) {
-                        workingNode.placePiece(Piece.WHITE);
-                    } else {
-                        workingNode.placePiece(Piece.BLACK);
-                    }
-                }
-                break;
-
-            //As GIPF pieces not are implemented yet, this is mode is yet to be implemented
-            case DOUBLE:
-                throw new Error("Handling of Gifp pieces not implemented yet");
-        }
+        SetupStartingPieces(startingMode, nodes, layers);
 
         // Generates and returns the final state
         return new GameState(startingPieces, nodes, layers);
@@ -135,5 +51,86 @@ public class StateFactory {
             }
         }
         return false;
+    }
+
+    private static void GenerateNodes(INode[] nodes, int[] layerEnds) {
+        //Generates the specific nodes and puts them in the nodes array
+        //uses a method to calculate whether its an outer or inner node
+        for (int i = 0; i < nodes.length; i++) {
+            if (OuterNode(i, layerEnds)) {
+                nodes[i] = new OuterNode();
+
+            } else {
+                nodes[i] = new InnerNode();
+            }
+        }
+    }
+
+    private static void AddNeighbors(INode[] nodes, int[] layerEnds){
+        //Loops through all the nodes and adds the neighbors on each side
+        for (int i = 0; i < nodes.length; i++) {
+            if (nodes[i] instanceof OuterNode) {
+                continue;
+            }
+            //Some math calculation as diagonal offsets change when the row-width starts to shrink again
+            int currentLayer = GetLayer(i, layerEnds) - 1;
+            int size = layerEnds[currentLayer] - layerEnds[currentLayer - 1];
+            int upModifier = (currentLayer > layerEnds.length/2 ? 1 : 0);
+            int downModifier = (currentLayer >= layerEnds.length/2 ? 1 : 0);
+            if (!nodes[i].hasNeighbor(Direction.LEFT)) {
+                nodes[i].addNeighbor(nodes[i - 1], Direction.LEFT);
+            }
+            if (!nodes[i].hasNeighbor(Direction.RIGHT)) {
+                nodes[i].addNeighbor(nodes[i + 1], Direction.RIGHT);
+            }
+            if (!nodes[i].hasNeighbor(Direction.UP_LEFT)) {
+                nodes[i].addNeighbor(nodes[i - size - upModifier], Direction.UP_LEFT);
+            }
+            if (!nodes[i].hasNeighbor(Direction.UP_RIGHT)) {
+                nodes[i].addNeighbor(nodes[i - size + 1 - upModifier], Direction.UP_RIGHT);
+            }
+            if (!nodes[i].hasNeighbor(Direction.DOWN_LEFT)) {
+                nodes[i].addNeighbor(nodes[i + size - downModifier], Direction.DOWN_LEFT);
+            }
+            if (!nodes[i].hasNeighbor(Direction.DOWN_RIGHT)) {
+                nodes[i].addNeighbor(nodes[i + size + 1 - downModifier], Direction.DOWN_RIGHT);
+            }
+        }
+    }
+
+    private static void SetupStartingPieces(StartingPieces startingMode, INode[] nodes, int layers) {
+        /*Used to specify the starting mode of the game:
+            None:
+                there will be placed no pieces on the board to start with
+            Normal:
+                in each "corner" of the board, there will be either a black or a white piece
+            Double:
+                in each "corner" of the board there will be a GIPF piece either black or white
+        */
+        switch (startingMode) {
+            //No pieces have to be placed
+            case NONE:
+                break;
+
+            //Finds each corner and places a normal piece
+            case NORMAL:
+                for (int i = 0; i < 6; i++) {
+                    INode workingNode = nodes[nodes.length / 2];
+
+                    for (int j = 0; j < layers; j++) {
+                        workingNode = workingNode.neighbor(Direction.fromValue(i));
+                    }
+                    if (i % 2 == 0) {
+                        workingNode.placePiece(Piece.WHITE);
+                    } else {
+                        workingNode.placePiece(Piece.BLACK);
+                    }
+                }
+                break;
+
+            //As GIPF pieces not are implemented yet, this is mode is yet to be implemented
+            case DOUBLE:
+                throw new Error("Handling of Gifp pieces not implemented yet");
+        }
     }
 }
