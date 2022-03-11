e769d2c074bae6ca0491659e6a6984888d7bf8e3
AngelofDeath666 <khejser@hotmail.com>
2022-03-11 15:53:51 +0100

    Readded outcommented code

--- src/ai/gr64/Main.java
+++ src/ai/gr64/Main.java
@@ -13,17 +13,16 @@ public class Main {
         //Generate nessesary resources
         GameState state = StateFactory.create(3, StartingPieces.NORMAL,15);
         var UI = new TextUI();
-        UI.UpdateUi(state);
 
         //The move-gens choose how to play the game, whether it is a player, some random AI, og a smart AI of some kind
-       // var moveGen1 = new RandomAI();
-        //var moveGen2 = new RandomAI();
+        var moveGen1 = new RandomAI();
+        var moveGen2 = new RandomAI();
 
         //Pass them to the game
-        // var game = new Engine(moveGen1, moveGen2,UI, state);
+        var game = new Engine(moveGen1, moveGen2,UI, state);
 
         //run the game
-        // game.Run();
+        game.Run();
     }
 
 }
