0a0f9c78642ecf4e6f951749127f6db0aee2613b
AngelofDeath666 <khejser@hotmail.com>
2022-03-14 13:39:53 +0100

    Added requested comments and added execptions

--- src/ai/gr64/Data/Interfaces/IUI.java
+++ src/ai/gr64/Data/Interfaces/IUI.java
@@ -1,15 +1,12 @@
 package ai.gr64.Data.Interfaces;
 
-import ai.gr64.Data.Enums.Direction;
 import ai.gr64.Engine.DTOs.GameState;
 import ai.gr64.Engine.DTOs.Move;
 
 // Interface for all implementations of a User-Interface
 public interface IUI {
-    public Move GetPlayerInput(boolean player1, Direction dir, int position);
+    public Move GetPlayerInput();
 
-    
     public void UpdateUi(GameState state);
-    
-   
+
 }
